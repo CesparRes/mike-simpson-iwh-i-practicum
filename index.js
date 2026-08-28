46a8698613e3a103daf248908ba65cc1c59fe979
Cesparres <mike.j.b.simpson@googlemail.com>
2026-08-28 17:01:35 +0200

    tidied up url parameters

--- index.js
+++ index.js
@@ -14,12 +14,14 @@ if (!process.env.PRIVATE_APP_ACCESS) {
 }
 
 const PRIVATE_APP_ACCESS = process.env.PRIVATE_APP_ACCESS;
+const HUBSPOT_OBJECT = '2-252531259';
+const HUBSPOT_BASE_URL = `https://api.hubspot.com/crm/v3/objects/`;
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
 
 // * Code for Route 1 goes here
-app.get('/studies', async (req, res) => {
-    const studies = 'https://api.hubspot.com/crm/v3/objects/2-252531259?properties=course_name,course_description,course_duration';
+app.get('/', async (req, res) => {
+    const studies = `${HUBSPOT_BASE_URL}${HUBSPOT_OBJECT}?properties=course_name,course_description,course_duration`;
     const headers = {
         Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
         'Content-Type': 'application/json'
@@ -41,6 +43,28 @@ app.get('/studies', async (req, res) => {
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
 
 // * Code for Route 2 goes here
+app.post('/update', async (req, res) => {
+    const update = {
+        properties: {
+            "favorite_book": req.body.newVal
+        }
+    }
+
+    const email = req.query.email;
+    const updateContact = `https://api.hubapi.com/crm/v3/objects/contacts/${email}?idProperty=email`;
+    const headers = {
+        Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+        'Content-Type': 'application/json'
+    };
+
+    try { 
+        await axios.patch(updateContact, update, { headers } );
+        res.redirect('back');
+    } catch(err) {
+        console.error(err);
+    }
+
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
 
